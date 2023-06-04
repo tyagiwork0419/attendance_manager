4bef9dab0855a6093f43fb798dc64915cc629cc2
tyagiwork0419 <tyagiwork0419@gmail.com>
2023-06-05 08:16:53 +0900

    implement get post command

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\tetuy\\fvm\\versions\\3.7.10" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\tetuy\\OneDrive\\documents\\FlutterProjects\\attendance_manager" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\tetuy\\fvm\\versions\\3.7.10"
+  "PROJECT_DIR=C:\\Users\\tetuy\\OneDrive\\documents\\FlutterProjects\\attendance_manager"
+  "FLUTTER_ROOT=C:\\Users\\tetuy\\fvm\\versions\\3.7.10"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\tetuy\\OneDrive\\documents\\FlutterProjects\\attendance_manager\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\tetuy\\OneDrive\\documents\\FlutterProjects\\attendance_manager"
+  "FLUTTER_TARGET=C:\\Users\\tetuy\\OneDrive\\documents\\FlutterProjects\\attendance_manager\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iNGZiMTEyMTRkZDJkZGE2Y2UwMTJkZDk4ZWE0OThlOWU4YjkxMjYyLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\tetuy\\OneDrive\\documents\\FlutterProjects\\attendance_manager\\.dart_tool\\package_config.json"
+)
