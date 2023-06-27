c16f6fa574c11ec3fe8045ad172d0ccab0c6b4d1
tyagiwork0419 <tyagiwork0419@gmail.com>
2023-06-28 02:04:56 +0900

    6/28

--- lib/models/attend_data.dart
+++ lib/models/attend_data.dart
@@ -1,6 +1,6 @@
 import 'package:intl/intl.dart';
 
-enum AttendType { clockIn, clockOut }
+enum AttendType { clockIn, clockOut, none }
 
 extension AttendTypeExtension on AttendType {
   String get toStr {
@@ -9,39 +9,82 @@ extension AttendTypeExtension on AttendType {
         return '出勤';
       case AttendType.clockOut:
         return '退勤';
+
+      case AttendType.none:
+        return '';
+    }
+  }
+
+  static AttendType toAttendType(String str) {
+    switch (str) {
+      case '出勤':
+        return AttendType.clockIn;
+      case '退勤':
+        return AttendType.clockOut;
+
+      default:
+        return AttendType.none;
     }
   }
 }
 
-enum Status { normal, deleted}
+enum Status { normal, deleted, none }
 
-extension StatusExtension on Status{
+extension StatusExtension on Status {
   String get toStr {
     switch (this) {
       case Status.normal:
         return 'normal';
       case Status.deleted:
         return 'deleted';
+
+      case Status.none:
+        return 'none';
+    }
+  }
+
+  static Status toStatus(String str) {
+    switch (str) {
+      case 'normal':
+        return Status.normal;
+      case 'deleted':
+        return Status.deleted;
+
+      default:
+        return Status.none;
     }
   }
 }
+
 class AttendData {
-  late int id;
+  late int id = 0;
   late String name;
   late AttendType type;
   late DateTime time;
-  late Status status;
+  late Status status = Status.normal;
 
-  AttendData(this.id, this.name, this.type, this.time, this.status);
+  final DateFormat _timeFormat = DateFormat('yyyy/MM/dd HH:mm:ss');
 
-  Map<String, String> toJson() {
-    DateFormat outputFormat = DateFormat('yyyy/MM/dd hh:mm:ss');
+  AttendData(this.name, this.type, this.time);
 
+  String get timeStr {
+    return _timeFormat.format(time);
+  }
+
+  AttendData.fromJson(Map<String, dynamic> jsonMap)
+      : id = jsonMap['id']!,
+        name = jsonMap['name']!,
+        type = AttendTypeExtension.toAttendType(jsonMap['type']!),
+        status = StatusExtension.toStatus(jsonMap['status']!) {
+    time = _timeFormat.parse(jsonMap['time']!);
+  }
+
+  Map<String, dynamic> toJson() {
     return {
       'id': id.toString(),
       'name': name,
       'type': type.toStr,
-      'time': outputFormat.format(time),
+      'time': timeStr,
       'status': status.toStr,
     };
   }
