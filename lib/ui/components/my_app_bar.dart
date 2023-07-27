5a515b808afc887597cb55115e359d7d66bddc6b
tyagiwork0419 <tyagiwork0419@gmail.com>
2023-07-27 12:12:32 +0900

    ver0.0.22

--- lib/ui/components/my_app_bar.dart
+++ lib/ui/components/my_app_bar.dart
@@ -4,27 +4,9 @@ import '../../application/constants.dart';
 
 class MyAppBar {
   final String title;
-  final String version;
+  final String? version;
 
-  const MyAppBar({required this.title, required this.version});
-
-/*
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      // Here we take the value from the MyHomePage object that was created by
-      // the App.build method, and use it to set our appbar title.
-      title: Text(title),
-      actions: [
-        Padding(
-            padding: const EdgeInsets.only(right: 30),
-            child: Align(
-                alignment: Alignment.centerRight,
-                child: _version(context, version)))
-      ],
-    );
-  }
-  */
+  const MyAppBar({required this.title, this.version});
 
   AppBar appBar(BuildContext context) {
     return AppBar(
@@ -32,11 +14,12 @@ class MyAppBar {
       // the App.build method, and use it to set our appbar title.
       title: Text(title),
       actions: [
-        Padding(
-            padding: const EdgeInsets.only(right: 30),
-            child: Align(
-                alignment: Alignment.centerRight,
-                child: _version(context, version)))
+        if (version != null)
+          Padding(
+              padding: const EdgeInsets.only(right: 30),
+              child: Align(
+                  alignment: Alignment.centerRight,
+                  child: _version(context, version!)))
       ],
     );
   }
